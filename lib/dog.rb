eb4aac32d25ba7816ebb42bb01125344a0e197d9
geoffrey woodcock <theatomproject@gmail.com>
2018-06-28 19:07:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,16 +3,12 @@ class Dog
   attr_accessor :name, :breed 
   attr_reader :id
   
-  # def initialize(id =nil, name:, breed:)
-  def initialize(h)
-     h.each {|k,v| public_send("#{k}=",v)}
+  def initialize(id =nil, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed 
+    
   end
-    
-  #   @id = id 
-  #   @name = name 
-  #   @breed = breed 
-    
-  # end
   
   def self.create_table
     sql =  <<-SQL 
